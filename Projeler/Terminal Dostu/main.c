34e1b1a60e64b91d4af9b4a576423c2cdd6b919e
trs-1342 <socialmediaoftrts1342@gmail.com>
2026-02-28 04:46:58 +0300

    Proje Duzenlendi: Terminal Dostu

--- Projeler/Terminal Dostu/main.c
+++ Projeler/Terminal Dostu/main.c
@@ -1,7 +1,39 @@
 #include <stdio.h>
 
-int main(int argc, char const *argv[])
+int main()
 {
-    /* code */
+    int secim, sayi1, sayi2, sonuc;
+    printf("MENU\n");
+    printf("====\n");
+    printf("1) +\n");
+    printf("2) -\n");
+    printf("3) *\n");
+    printf("4) /\n");
+
+    printf("Secim: ");
+    scanf("%d", &secim);
+
+    printf("\nsayi 1: ");
+    scanf("%d", &sayi1);
+
+    printf("\nsayi 2: ");
+    scanf("%d", &sayi2);
+
+    if (secim==1) {
+        sonuc=sayi1+sayi2;
+        printf("\n+ islemin sonucu: %d + %d = %d\n\n", sayi1, sayi2, sonuc);
+    } else if (secim==2) {
+        sonuc=sayi1-sayi2;
+        printf("\n- islemin sonucu: %d - %d = %d\n\n", sayi1, sayi2, sonuc);
+    } else if (secim==3) {
+        sonuc=sayi1*sayi2;
+        printf("\n* islemin sonucu: %d * %d = %d\n\n", sayi1, sayi2, sonuc);
+    } else if (secim==4) {
+        sonuc=sayi1/sayi2;
+        printf("\n/ islemin sonucu: %d / %d = %d\n\n", sayi1, sayi2, sonuc);
+    } else {
+        printf("\t\nBir sorun olustu\n\n");
+    }
+
     return 0;
 }
